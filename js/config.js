ccb098e1e7620cf0f24768176d10d428688d3072
damonxuda <damon_xuda@163.com>
2025-07-03 13:32:15 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 // js/config.js
-window.supabaseUrl = 'https://exnfrclagndigmdybicg.supabase.co';
-window.supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4bmZyY2xhZ25kaWdtZHliaWNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1MTEyODIsImV4cCI6MjA2NzA4NzI4Mn0.aW0snMCgrrEV4LOF64JGM0qD324o_WvvYxNFoPXPHgY';
+window.supabaseUrl = 'https://exnfrclagndigmdybicq.supabase.co';
+window.supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4bmZyY2xhZ25kaWdtZHliaWNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4NzA5MDAsImV4cCI6MjA1MTQ0NjkwMH0.aW0snMCgrrEV4LOF64JGM0qD324o_WvvYxNFoPXPHgY';
 
 // 初始化客户端
 document.addEventListener('DOMContentLoaded', function() {
-  if (window.supabase && window.supabase.createClient) {
-    window.supabase = supabase;    
-  }
+    if (supabase && supabase.createClient) {
+        window.supabase = supabase.createClient(window.supabaseUrl, window.supabaseKey);
+    }
 });
