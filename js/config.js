71c2a16c41cba4f5ac3cd3804970d0b5c57b59a8
damonxuda <damon_xuda@163.com>
2025-07-03 20:28:15 +0800

    Update config.js -- update v2
    summer-course-schedule-v2

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // js/config.js
-window.supabaseUrl = 'https://exnfrclagndigmdybicq.supabase.co';
-window.supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4bmZyY2xhZ25kaWdtZHliaWNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4NzA5MDAsImV4cCI6MjA1MTQ0NjkwMH0.aW0snMCgrrEV4LOF64JGM0qD324o_WvvYxNFoPXPHgY';
+window.supabaseUrl = 'https://wytqlpwlelznkoxhygfc.supabase.co';
+window.supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5dHFscHdsZWx6bmtveGh5Z2ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1Mzk4NDAsImV4cCI6MjA2NzExNTg0MH0.bMPiovIGFAqdC-BQwozDBN9FhbCPwszwG9gUJ-oc7Ok';
 
 // 初始化客户端
 document.addEventListener('DOMContentLoaded', function() {
