e382f71d6acd6fb3bae627efd570836f4900733c
damonxuda <damon_xuda@163.com>
2025-07-03 11:26:48 +0800

    Update config.js -- 20250703 v0.1
    增加初始化客户端的代码

--- js/config.js
+++ js/config.js
@@ -1,3 +1,10 @@
 // js/config.js
 window.supabaseUrl = 'https://exnfrclagndigmdybicg.supabase.co';
 window.supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4bmZyY2xhZ25kaWdtZHliaWNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1MTEyODIsImV4cCI6MjA2NzA4NzI4Mn0.aW0snMCgrrEV4LOF64JGM0qD324o_WvvYxNFoPXPHgY';
+
+// 初始化客户端
+document.addEventListener('DOMContentLoaded', function() {
+  if (window.supabase && window.supabase.createClient) {
+    window.supabase = window.supabase.createClient(window.supabaseUrl, window.supabaseKey);
+  }
+});
