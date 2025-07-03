1d4c822a587176264dfb7aa66450370803a32c88
damonxuda <damon_xuda@163.com>
2025-07-03 21:23:39 +0800

    Update config.js -- update 20250703 v3
    设置使用supabase模式

--- js/config.js
+++ js/config.js
@@ -2,6 +2,9 @@
 window.supabaseUrl = 'https://wytqlpwlelznkoxhygfc.supabase.co';
 window.supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5dHFscHdsZWx6bmtveGh5Z2ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1Mzk4NDAsImV4cCI6MjA2NzExNTg0MH0.bMPiovIGFAqdC-BQwozDBN9FhbCPwszwG9gUJ-oc7Ok';
 
+// 设置使用Supabase模式
+window.useSupabase = true;
+
 // 初始化客户端
 document.addEventListener('DOMContentLoaded', function() {
     if (supabase && supabase.createClient) {
